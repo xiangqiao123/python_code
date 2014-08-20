9b822affe32def4a749f2bb9ffefcba032b5707d
hunter <5727469094@qq.com>
2014-08-20 20:34:26 +0800

    list

---
+++ list_multiply.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+sentence=raw_input("input sentence:")
+
+screen_width=80
+text_width=len(sentence)
+box_width=text_width+6
+left_margin=(screen_width-text_width)//2
+
+print
+print " " * left_margin + "+" + "-" * (box_width-2) + "+"
+print " " * left_margin + "|  " + " " * text_width + "  |"
+print " " * left_margin + "|  " +sentence + "  |"
+print " " * left_margin + "|  " + " " * text_width + "  |"
+print " " * left_margin + "+" + "-" * (box_width-2) + "+"
---
+++ list_split.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+url=raw_input("please input url:")
+domain=url[11:-4]
+
+print "Domain name:"+domain
